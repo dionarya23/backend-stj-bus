632201903f50ccd2c8503d3496d9fc64c4e11f53
Dion Arya Pamungkas <dionarya.p@gmail.com>
2021-01-03 12:29:17 +0700

    add token beare statistik endpoint

--- routes/statistik.js
+++ routes/statistik.js
@@ -2,7 +2,8 @@ const baseRoute = require("./base_router");
 const baseController = require("../controllers/base.controller");
 const statistikController = require("../controllers/statistik.controller");
 const router = baseRoute();
+const authAdminMiddleware = require("../middleware/authAdmin.middleware");
 
-router.get("/bulan/:bulan/:tahun", baseController(req => statistikController.getStatistikBulan(req)));
+router.get("/bulan/:bulan/:tahun", authAdminMiddleware, baseController(req => statistikController.getStatistikBulan(req)));
 
 module.exports = router;
