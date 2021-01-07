5a1684ef4a2a3724826ea76c3476573d602d2d5d
Dion Arya Pamungkas <dionarya.p@gmail.com>
2021-01-07 21:18:01 +0700

    add middleware for endpoint check qrcode

--- routes/passengers.js
+++ routes/passengers.js
@@ -3,9 +3,9 @@ const baseController = require("../controllers/base.controller")
 const orderController = require("../controllers/passengers.controller")
 const router = baseRoute()
 
-// const driverAuthMiddleware = require("../middleware/authSupir.middleware")
+const driverAuthMiddleware = require("../middleware/authSupir.middleware")
 
 router.post("/seat", baseController(req => orderController.check(req)))
-router.get("/check/:order_id", baseController(req => orderController.checkPassenger(req)));
+router.get("/check/:order_id", driverAuthMiddleware, baseController(req => orderController.checkPassenger(req)));
 
 module.exports = router
