20717f8c113e361c1fbcca9907c3a536d755e117
Dion Arya Pamungkas <dionarya.p@gmail.com>
2021-01-10 13:15:14 +0700

    add mekanik middleware at endpoint

--- routes/stock_bengkel.js
+++ routes/stock_bengkel.js
@@ -4,9 +4,23 @@ const baseController = require("../controllers/base.controller");
 const stockBengkelController = require("../controllers/stockBengkel.controller");
 
 const router = baseRoute();
+const adminMiddleware = require("../middleware/authAdmin.middleware");
+const mekanikMiddleware = require("../middleware/authMekanik.middleware");
 
-router.post("/", baseController(req => stockBengkelController.createStock(req)));
-router.put("/", baseController(req => stockBengkelController.updateStockSukuCadang(req)));
-router.get("/bengkel/:id_bengkel", baseController(req => stockBengkelController.getStockByIdBengkel(req)));
+router.post(
+  "/",
+  adminMiddleware,
+  baseController((req) => stockBengkelController.createStock(req))
+);
+router.put(
+  "/",
+  adminMiddleware,
+  baseController((req) => stockBengkelController.updateStockSukuCadang(req))
+);
+router.get(
+  "/bengkel/:id_bengkel",
+  mekanikMiddleware,
+  baseController((req) => stockBengkelController.getStockByIdBengkel(req))
+);
 
-module.exports = router
+module.exports = router;
